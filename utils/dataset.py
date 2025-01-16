ce8de806d4f0212915113c4adbbc0639ff0d3645
Enrique Morales <enrique.morales@uni-bonn.de>
2025-01-16 09:18:05 +0100

    Dataset class for xgaze dataset - optional normalization - front facing images only

--- utils/dataset.py
+++ utils/dataset.py
@@ -2,6 +2,7 @@ from torch.utils.data import Dataset
 from tqdm import tqdm
 import h5py
 import json
+from torchvision import transforms
 import torch
 
 path_to_xgaze = "../Datasets/xgaze_224/"
@@ -13,23 +14,47 @@ with open(path_to_xgaze + "train_test_split.json", 'r') as file:
 def get_train_data():
     images = []
     gaze_labels = []
-    for filename in tqdm(data['train'][0], "Loading image and Gaze Data"):
+    # for filename in tqdm(data['train'], "Loading image and Gaze Data"):
+    for filename in tqdm(data['train'], "Loading image and Gaze Data"):
         path = path_to_xgaze + "train/" + filename
         with h5py.File(path) as subject:
             # Only frontal view is required.  
             # With 18 cameras only the picture of the first
             images.append(
                 torch.from_numpy(
-                    subject['face_patch'][0::18]))
+                    subject['face_patch'][0::18]).float()/255.0)
+
+            # Images are stored in W x H x C
+            # but torch expects C x W x H
+            images[-1] = images[-1].permute(0,3,2,1)
+            
             gaze_labels.append(
                 torch.from_numpy(
-                    subject['face_gaze'][0::18]))
+                    subject['face_gaze'][0::18]).float())
 
     return (torch.cat(images, dim=0), torch.cat(gaze_labels, dim=0))
 
+def normalize_images(images):
+    """
+    Normalize image for ResNet to the range [0, 1]
+    with: 
+    mean=[0.485, 0.456, 0.406] 
+    std=[0.229, 0.224, 0.225]
+    """
+    # Normalize image for ResNet
+    # with: 
+    mean=[0.485, 0.456, 0.406] 
+    std=[0.229, 0.224, 0.225]
+    transform = transforms.Compose([
+        transforms.Normalize(mean,std),
+    ])
+    return transform(images)
+
 class GazeImages(Dataset):
-    def __init__(self):
+    def __init__(self, normalize: bool = False):
         self.images, self.gaze_labels = get_train_data()
+        if normalize:
+            self.images = normalize_images(self.images)
         return
 
     def __len__(self):
