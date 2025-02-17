502be14911cf5fb03bca30fb2401caff9c28c662
Enrique Morales <enrique.morales@uni-bonn.de>
2025-02-17 13:05:09 +0100

    add way to load with mask and data augmentation

--- utils/dataset.py
+++ utils/dataset.py
@@ -3,7 +3,8 @@ import pickle
 from tqdm import tqdm
 import h5py
 import json
-from torchvision import transforms
+import torchvision.transforms.v2.functional as F
+from torchvision.transforms.v2 import GaussianBlur, RandomCrop, ColorJitter, GaussianNoise
 from itertools import combinations_with_replacement
 import torch
 
@@ -94,10 +95,102 @@ def inv_normalize_images(images):
 #         label = self.gaze_labels[idx, ...]
 #         return image, label 
 
-class GazeImages_forUNet(Dataset):
-    def __init__(self, normalize: bool = False):
+class same_color_jitter():
+    def __init__(self, order: torch.Tensor, brightness, contrast, saturation, hue):
+        self.order = order
+        self.brightness = brightness
+        self.contrast = contrast
+        self.saturation = saturation
+        self.hue = hue
+
+    def __call__(self, img):
+        for fn_id in self.order:
+            if fn_id == 0 and self.brightness is not None:
+                img = F.adjust_brightness(img, self.brightness)
+            elif fn_id == 1 and self.contrast is not None:
+                img = F.adjust_contrast(img, self.contrast)
+            elif fn_id == 2 and self.saturation is not None:
+                img = F.adjust_saturation(img, self.saturation)
+            elif fn_id == 3 and self.hue is not None:
+                img = F.adjust_hue(img, self.hue)
+
+        return img
+
+class GazeImages_forUNet_pre_pro(Dataset):
+    def __init__(self, normalize: bool = False, train = True, augmentation = True, heatmap = False, path_to_xgaze = "../Datasets/xgaze_set/"):
         # self.images, self.gaze_labels, self.subject_index = get_train_data()
 
+        self.augmentation = augmentation
+        self.heatmap = heatmap
+        self.train = train
+        self.normalize = normalize
+        self.path_to_xgaze = path_to_xgaze
+
+        with open(self.path_to_xgaze + "train_test_split.json", 'r') as file:
+            self.subject_index = json.load(file)
+
+        ### load list of every possible combination of image-indicies within a subject
+        with open(self.path_to_xgaze + "combination_list.pickle", "rb") as f:
+            self.combination_list = pickle.load(f)
+
+    def __len__(self):
+        return len(self.combination_list)
+
+    def __getitem__(self, idx):
+        subject_idx, start_idx, target_idx = self.combination_list[idx]
+        if self.train:
+            path = self.path_to_xgaze + "train/" + self.subject_index['train'][subject_idx]
+
+            with h5py.File(path) as subject:
+                # Only frontal view is required.  
+                # With 18 cameras only the picture of the first
+                imgages = subject['face_patch']
+                gazes = subject['face_gaze']
+                # combine eye masks
+                mask = torch.from_numpy(subject['left_eye_mask'][target_idx]) + torch.from_numpy(subject['right_eye_mask'][target_idx])
+                image_start = torch.from_numpy(imgages[start_idx]).float()/255.0
+                label_start = torch.from_numpy(gazes[start_idx]).float()
+                image_target = torch.from_numpy(imgages[target_idx]).float()/255.0
+                label_target = torch.from_numpy(gazes[target_idx]).float()
+
+        # Images are stored in W x H x C
+        # but torch expects C x W x H
+        image_start = image_start.permute(2,0,1)
+        image_target = image_target.permute(2,0,1)
+        
+        # dateset augmentation
+        if self.augmentation:
+        # combine images, along channels in order to have the same transform
+            combined = torch.cat((image_start, image_target, mask.expand(1, -1, -1)), dim=0)
+            # spacial augmentation
+            crop = RandomCrop(size=432)(combined)
+
+            # color augmentation TODO augmentation for brightness
+            generator = ColorJitter(brightness=.7, hue=.5)
+            color = same_color_jitter(*ColorJitter.get_params(generator.brightness, generator.contrast, generator.saturation,generator.hue))
+
+            image_start = color(crop[0:3,...])
+            image_target = color(crop[3:6,...])
+            mask = crop[6:, ...]
+            # augmentation for camera noise
+            noise = torch.rand_like(image_start)*.1
+            image_start = torch.clamp(image_start + noise, 0, 1)
+            image_target = torch.clamp(image_target + noise, 0, 1)
+
+        if self.heatmap:
+            mask = GaussianBlur(kernel_size=(201,103), sigma = 20.9)(mask)
+        # Normalize images
+        if self.normalize:
+            image_start = normalize_images(image_start)
+            image_target = normalize_images(image_target)
+
+        return image_start, label_start, image_target, label_target, mask
+
+class GazeImages_forUNet(Dataset):
+    def __init__(self, normalize: bool = False, train = True):
+        # self.images, self.gaze_labels, self.subject_index = get_train_data()
+
+        self.train = train
         self.normalize = normalize
         self.path_to_xgaze = "../Datasets/xgaze_224/"
 
@@ -114,17 +207,31 @@ class GazeImages_forUNet(Dataset):
 
     def __getitem__(self, idx):
         subject_idx, start_idx, target_idx = self.combination_list[idx]
-        path = self.path_to_xgaze + "train/" + self.subject_index['train'][subject_idx]
+        if self.train:
+            path = self.path_to_xgaze + "train/" + self.subject_index['train'][subject_idx]
 
-        with h5py.File(path) as subject:
-            # Only frontal view is required.  
-            # With 18 cameras only the picture of the first
-            imgages = subject['face_patch']
-            gazes = subject['face_gaze']
-            image_start = torch.from_numpy(imgages[18*start_idx]).float()/255.0
-            label_start = torch.from_numpy(gazes[18*start_idx]).float()
-            image_target = torch.from_numpy(imgages[18*target_idx]).float()/255.0
-            label_target = torch.from_numpy(gazes[18*target_idx]).float()
+            with h5py.File(path) as subject:
+                # Only frontal view is required.  
+                # With 18 cameras only the picture of the first
+                imgages = subject['face_patch']
+                gazes = subject['face_gaze']
+                image_start = torch.from_numpy(imgages[18*start_idx]).float()/255.0
+                label_start = torch.from_numpy(gazes[18*start_idx]).float()
+                image_target = torch.from_numpy(imgages[18*target_idx]).float()/255.0
+                label_target = torch.from_numpy(gazes[18*target_idx]).float()
+
+        else:
+            path = self.path_to_xgaze + "test/" + self.subject_index['test'][subject_idx]
+
+            with h5py.File(path) as subject:
+                # Only frontal view is required.  
+                # With 18 cameras only the picture of the first
+                imgages = subject['face_patch']
+                gazes = subject['face_gaze']
+                image_start = torch.from_numpy(imgages[18*start_idx]).float()/255.0
+                label_start = torch.from_numpy(gazes[18*start_idx]).float()
+                image_target = torch.from_numpy(imgages[18*target_idx]).float()/255.0
+                label_target = torch.from_numpy(gazes[18*target_idx]).float()
 
         # Images are stored in W x H x C
         # but torch expects C x W x H
@@ -138,15 +245,14 @@ class GazeImages_forUNet(Dataset):
 
         return image_start, label_start, image_target, label_target 
 if __name__ == "__main__":
-    with open("subject_indices.json", 'r') as file:
-        subject_index = json.load(file)
 
     combination_list = []
-
-    for i in range(len(subject_index)-1):
-        a = range(subject_index[i + 1] - subject_index[i])
-        combination_list.extend([(i,) + t for t in combinations_with_replacement(a, 2)])
+    for idx, subject in enumerate(tqdm(subjects)): 
+        with h5py.File(subject) as s:
+            # Only frontal view is required.
+            # e.g. cameras with index == 0
+            a = [i for i, c in enumerate(s['cam_index']) if c == 0]
+        combination_list.extend([(idx,) + t for t in combinations_with_replacement(a, 2)])
 
     with open("combination_list.pickle", "wb") as f:
         pickle.dump(combination_list, f)
-
